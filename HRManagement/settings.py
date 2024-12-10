e170e3b70e1846d007022ed4d20149090440ab50
zubair-smd <zubaircdp23@gmail.com>
2024-12-10 21:05:31 +0000

    update

--- HRManagement/settings.py
+++ HRManagement/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
    'django.middleware.security.SecurityMiddleware',
+   'whitenoise.middleware.WhiteNoiseMiddleware',
    'django.contrib.sessions.middleware.SessionMiddleware',
    'django.middleware.common.CommonMiddleware',
    'django.middleware.csrf.CsrfViewMiddleware',
@@ -105,7 +106,8 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'employees/static')
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 # Static files finder configuration
 STATICFILES_FINDERS = [
     'django.contrib.staticfiles.finders.FileSystemFinder',
